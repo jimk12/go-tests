f41bf682b076946e9e8aeacf55c2a87b2c0c13f4
Jim Kettunen <jim@Jims-MacBook-Air.local>
2018-08-24 01:43:25 -0700

    Added easyMath package

---
+++ goMath/easyMath.go
@@ -0,0 +1,17 @@
+package goMath
+
+func Add(x,y int) int {
+	return x+y
+}
+
+func Sub(x,y int) int {
+	return x-y
+}
+
+func Mult(x,y int) int {
+	return x*y
+}
+
+func Div(x,y float64) float64 {
+	return x/y
+}
--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,12 @@
 package main
 
 import "fmt"
+import "git/go-tests/goMath"
 
 func main() {
 	fmt.Println("Hello World")
+	fmt.Println(goMath.Add(2,5))
+	fmt.Println(goMath.Sub(2,5))
+	fmt.Println(goMath.Mult(2,5))
+	fmt.Println(goMath.Div(2,5))
 }
